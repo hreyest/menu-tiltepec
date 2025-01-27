264df8b0e220ebea2b27231b0db44b0c96f87785
retrevigno <retrevigno@gmail.com>
2025-01-27 00:49:24 -0600

    primera version

---
+++ script.js
@@ -0,0 +1,14 @@
+function showCategory(categoryId) {
+    // Oculta todas las categorías
+    document.querySelectorAll('.category').forEach(category => {
+        category.classList.remove('active');
+    });
+
+    // Muestra la categoría seleccionada
+    document.getElementById(categoryId).classList.add('active');
+}
+
+// Mostrar la categoría de entradas por defecto al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    showCategory('entradas');
+});
